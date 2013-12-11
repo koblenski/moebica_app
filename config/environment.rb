fcaedf840c36ccf4c84dea38933e0434a0e837ab
Sam Koblenski <koblenski@gmail.com>
2013-12-10 22:09:49 -0600

    Make a basic User model (including secure passwords)

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+# Make a shortcut for the find method on models
+class <<ActiveRecord::Base
+  alias_method :[], :find
+end
